3ba1902a4c0f2f8b4fbb1709306cfd5e4e165b39
ashashishy <ash.ashish.y@gmail.com>
2020-08-05 06:00:20 +0530

    bfs dfs and binary heap

---
+++ binaryHeap.js
@@ -0,0 +1,21 @@
+class MaxBinaryHeapr{
+    constructor(){
+        this.values=[];
+    }
+    insert(element){
+        this.values.push(element);
+        this.bubbleUP();
+    }
+    bubbleUp(){
+        let idx=this.values.length-1;
+        const element=this.values[idx];
+        while(true){
+            let parentIdx=Math.floor((idx-1)/2);
+            let parent=this.values[parentIdx];
+            if(element<=parent)break;
+            this.values[parentIdx]=element;
+            this.values[idx]=parent;
+            idx=parentIdx;
+        }
+    }
+}
--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,60 +1,108 @@
-class Node{
-	constructor(value){
-		this.value=value;
-		this.left=null;
-		this.right=null;
-	}
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
 }
 
-class BinarySearchTree{
-	constructor(){
-		this.root=null;
-	}
-	insert(value){
-		var newNode=new Node(value);
-		if(this.root===null){
-			this.root=newNode;
-			return this;
-		}
-		var current=this.root;
-		while(true){
-			if(value===current.value) return undefined;
-			if(value<current.value){
-				if(current.left===null){
-					current.left=newNode;
-					return this;
-				}else{
-					current=current.left;
-				}
-			}else{
-				if(current.right===null){
-					current.right=newNode;
-					return this;
-				}else{
-					current=current.right;
-				}
-			}				
-		}
-		
-	}
-	find(value){
-		if(this.root===null) return false;
-		var current=this.root;
-		var found=false;
-		while(current &&!found){
-			if(value<current.value){
-				current=current.left;
-			}else if(value>current.value){
-				current=current.right;
-			}else{
-				return true;
-			}
-		}
-		return false;
-	}
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    insert(value) {
+        var newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+            return this;
+        }
+        var current = this.root;
+        while (true) {
+            if (value === current.value) return undefined;
+            if (value < current.value) {
+                if (current.left === null) {
+                    current.left = newNode;
+                    return this;
+                } else {
+                    current = current.left;
+                }
+            } else {
+                if (current.right === null) {
+                    current.right = newNode;
+                    return this;
+                } else {
+                    current = current.right;
+                }
+            }
+        }
+
+    }
+    find(value) {
+        if (this.root === null) return false;
+        var current = this.root;
+        var found = false;
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+    BFS() {
+        var queue = [];
+        var data = [];
+        var node = this.root;
+        queue.push(node);
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
+
+    DFSPreOrder() {
+        var data = [];
+        var current = this.root;
+        traverse(node) {
+            data.push(node.value);
+            if (data.left) traverse(data.left);
+            if (data.right) traverse(data.right);
+
+        }
+        traverse(current);
+        return data;
+    }
+    DFSPostOrder() {
+        var data = [];
+        var current = this.root;
+        traverse(node) {
+            if (data.left) traverse(data.left);
+            if (data.right) traverse(data.right);
+            data.push(node.value);
+        }
+        traverse(current);
+        return data;
+    }
+    DFSInOrder() {
+        var data = [];
+        var current = this.root;
+        traverse(node) {
+            if (data.left) traverse(data.left);
+            data.push(node.value);
+            if (data.right) traverse(data.right);
+        }
+        traverse(current);
+        return data;
+    }
 };
- 
-var tree=new BinarySearchTree();
+
+var tree = new BinarySearchTree();
 tree.insert(20);
 tree.insert(15);
 tree.insert(12);
