4a7b7ef43a25aba2efe2b02dfbfc6943f434fc59
ashashishy <ash.ashish.y@gmail.com>
2020-08-07 06:35:57 +0530

    hash table implementation

---
+++ hashTable.js
@@ -0,0 +1,61 @@
+class HashTable {
+    construtor(size = 53) {
+        this.keyMap = new Array(size);
+    }
+
+    _hash(key) {
+        let total = 0;
+        let prime = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96;
+            total = (total * prime + value) % this.keyMap.length;
+        }
+        return total;
+    }
+    set(key, value) {
+        let index = this._hash(key);
+        if (!this.keyMap[index]) {
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, value])
+    }
+    get(key) {
+        let index = this._hash(key);
+        if (this.keyMap[index]) {
+            for (let i = 0; i < this.keyMap[index].length; i++) {
+                if (this.keyMap[index][i] === key) {
+                    return this.keyMap[index][i][1];
+                }
+            }
+        }
+        return undefined;
+    }
+    values() {
+        let arrValues = [];
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                for (let j = 0; j < this.keyMap[i].length; j++) {
+                    if (!arrValues.includes(this.keyMap[i][j][1])) {
+                        arrValues.push(this.keyMap[i][j][1]);
+                    }
+                }
+            }
+        }
+        return arrValues;
+    }
+    
+    keys() {
+        let arrKeys = [];
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                for (let j = 0; j < this.keyMap[i].length; j++) {
+                    if (!arrKeys.includes(this.keyMap[i][j][0])) {
+                        arrKeys.push(this.keyMap[i][j][0]);
+                    }
+                }
+            }
+        }
+        return arrKeys;
+    }
+}
